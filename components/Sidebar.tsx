b3bc5eb84075d20bcd4b9b48e70f9e273c7a9a3b
Horacio23 <horacio.delgado23@gmail.com>
2024-01-05 16:25:37 -0500

    fixed sidebar text size

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,7 +7,7 @@ const Sidebar = () => {
         <div>
             <ul className="flex flex-col h-full gap-2 my-8">
                 {NAV_LINKS.map((link) =>(
-                    <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexStart cursor-pointer pb-1.5 transition-all hover:font-bold active:font-bold active:bold-16 hover:border-r-2 hover:border-r-gray-800">
+                    <Link href={link.href} key={link.key} className="regular-20 text-gray-50 flexStart cursor-pointer pb-1.5 transition-all  hover:font-bold hover:border-r-2 hover:border-r-gray-800">
                         {link.label}
                     </Link>
                 ))}
